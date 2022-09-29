bc5baceb10aadb574300b0f9f0ff51f569f74644
lucas van eijnsbergen <99073105@mydavinci.nl>
2022-09-30 00:21:06 +0200

    de Speelhal-dag.py

--- mod_2/feestlunch.py
+++ mod_2/feestlunch.py
@@ -8,4 +8,3 @@ kortingsprijs = 0.50
 prijs = (croisantjes * prijsproduct + stokbroden * prijsproduct1 - kortingsbonnen * kortingsprijs )
 
 print("de feestlunch kost",prijs ,"euro voor de" ' ',croisantjes, ' ' "croisantjes en de" ' ',stokbroden, ' ' "stokbroden als de" ' ' ,kortingsbonnen, ' ' "korting bonnen nog geldig zijn")
-git commit
