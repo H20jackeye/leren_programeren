d4c71a4e901886bf7e034322d17203f1ad2042be
lucas van eijnsbergen <99073105@mydavinci.nl>
2022-10-03 14:06:41 +0200

    refresh mod 2

--- mod_2/de Speelhal-dag.py
+++ mod_2/de Speelhal-dag.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Dat kost een toegangsticket per persoon van 7,45 euro voor de hele dag.
 #Jullie willen ook met zijn allen voor 45 minuten in de VIP-VR-GameSeat.
 #De VIP-VR GameSeat kost per persoon 37 eurocent per 5 minuten.
@@ -7,41 +6,17 @@ antwoord = True or False
 persoonen = input 
 dag_kaart_per_persoon = 7.45
 vijf_minuten = 0.37
-
+een_minuut = (vijf_minuten / 5)
 persoonen = int(input("met hoe veel persoonen bent u "))
 prijs1 = (dag_kaart_per_persoon * persoonen)
 antwoord = input("willen jullie ook nog naar de VIP_VR_SEAT")
 
 if antwoord == "ja":
     tijd = float(input("hoe lang willen jullie"))
-    prijs2 = (vijf_minuten * tijd)
+    prijs2 = (een_minuut * tijd * persoonen)
     prijs = (prijs1 + prijs2)
-    print(prijs)
+    print("Dit geweldige dagje-uit met" ' ',persoonen , ' ' "mensen in de speelhal met" ' ',tijd ,' ' "minuten VR kost je maar" ' ' ,prijs , ' ' "euro")
 
 if antwoord == "nee":
-    print(prijs1)
-
-=======
-#Dat kost een toegangsticket per persoon van 7,45 euro voor de hele dag.
-#Jullie willen ook met zijn allen voor 45 minuten in de VIP-VR-GameSeat.
-#De VIP-VR GameSeat kost per persoon 37 eurocent per 5 minuten.
-#Jij trakteert dus betaal je alles.
-antwoord = True or False
-persoonen = input 
-dag_kaart_per_persoon = 7.45
-vijf_minuten = 0.37
-
-persoonen = int(input("met hoe veel persoonen bent u "))
-prijs1 = (dag_kaart_per_persoon * persoonen)
-antwoord = input("willen jullie ook nog naar de VIP_VR_SEAT")
-
-if antwoord == "ja":
-    tijd = float(input("hoe lang willen jullie"))
-    prijs2 = (vijf_minuten * tijd)
-    prijs = (prijs1 + prijs2)
-    print(prijs)
-
-if antwoord == "nee":
-    print(prijs1)
-
->>>>>>> 3960045d48bd0107ca39c04eb978f49169f752b2
+    print("dit geweldige dagje-uit met" ' ',persoonen , ' ' "mensen in de speelhal kost je maar" ' ' ,prijs1 ,' ' "euro" )
+#‘Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro’
